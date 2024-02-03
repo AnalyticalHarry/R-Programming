cbab1f02fa5fde8657f98f939fad996bb547397b
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-02-03 23:48:38 +0000

    Add files via upload

---
+++ 4 Arithmetic operations in R .R
@@ -0,0 +1,32 @@
+## Author: Hemant Thapa
+## Programming Language: R Programming
+## Variables in R 
+## Date Pushed to GitHub: 03.02.2024
+## Email: hemantthapa1998@gmail.com
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - -
+  # integers
+  num_integer_1 <- 5L
+  num_integer_2 <- 3L
+  
+  # addition
+  add_integers <- num_integer_1 + num_integer_2
+  cat("Addition of integers:", add_integers)
+  
+  # subtraction
+  subtract_integers <- num_integer_1 - num_integer_2
+  cat("Subtraction of integers:", subtract_integers)
+  
+  # multiplication
+  multiply_integers <- num_integer_1 * num_integer_2
+  cat("Multiplication of integers:", multiply_integers)
+  
+  # division
+  divide_integers <- num_integer_1 / num_integer_2
+  cat("Division of integers:", divide_integers)
+  
+  # modulus (remainder of division)
+  modulus_integers <- num_integer_1 %% num_integer_2
+  cat("Modulus (remainder of division) of integers:", modulus_integers)
+  
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - -
