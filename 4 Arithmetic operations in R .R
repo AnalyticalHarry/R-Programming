d72aa50f0f8a43140c5cc1cc3a81ba162449ce51
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-02-03 23:50:17 +0000

    Update 4 Arithmetic operations in R .R

--- 4 Arithmetic operations in R .R
+++ 4 Arithmetic operations in R .R
@@ -1,32 +1,72 @@
 ## Author: Hemant Thapa
 ## Programming Language: R Programming
-## Variables in R 
+## Arithmetic in R 
 ## Date Pushed to GitHub: 03.02.2024
 ## Email: hemantthapa1998@gmail.com
 
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - -
-  # integers
-  num_integer_1 <- 5L
-  num_integer_2 <- 3L
-  
-  # addition
-  add_integers <- num_integer_1 + num_integer_2
-  cat("Addition of integers:", add_integers)
-  
-  # subtraction
-  subtract_integers <- num_integer_1 - num_integer_2
-  cat("Subtraction of integers:", subtract_integers)
-  
-  # multiplication
-  multiply_integers <- num_integer_1 * num_integer_2
-  cat("Multiplication of integers:", multiply_integers)
-  
-  # division
-  divide_integers <- num_integer_1 / num_integer_2
-  cat("Division of integers:", divide_integers)
-  
-  # modulus (remainder of division)
-  modulus_integers <- num_integer_1 %% num_integer_2
-  cat("Modulus (remainder of division) of integers:", modulus_integers)
-  
+# integers
+num_integer_1 <- 5L
+num_integer_2 <- 3L
+
+# addition
+add_integers <- num_integer_1 + num_integer_2
+cat("Addition of integers:", add_integers)
+
+# subtraction
+subtract_integers <- num_integer_1 - num_integer_2
+cat("Subtraction of integers:", subtract_integers)
+
+# multiplication
+multiply_integers <- num_integer_1 * num_integer_2
+cat("Multiplication of integers:", multiply_integers)
+
+# division
+divide_integers <- num_integer_1 / num_integer_2
+cat("Division of integers:", divide_integers)
+
+# modulus (remainder of division)
+modulus_integers <- num_integer_1 %% num_integer_2
+cat("Modulus (remainder of division) of integers:", modulus_integers)
+
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - -
+# doubles
+num_double_1 <- 5.5
+num_double_2 <- 2.0
+
+# addition
+add_doubles <- num_double_1 + num_double_2
+cat("Addition of doubles:", add_doubles, "\n")
+
+# subtraction
+subtract_doubles <- num_double_1 - num_double_2
+cat("Subtraction of doubles:", subtract_doubles, "\n")
+
+# multiplication
+multiply_doubles <- num_double_1 * num_double_2
+cat("Multiplication of doubles:", multiply_doubles, "\n")
+
+# division
+divide_doubles <- num_double_1 / num_double_2
+cat("Division of doubles:", divide_doubles, "\n")
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - - - - -
+# Complex Numbers
+complex_num_1 <- 2 + 3i
+complex_num_2 <- 1 - 2i
+
+# addition
+add_complex <- complex_num_1 + complex_num_2
+cat("Addition of complex numbers:", add_complex, "\n")
+
+# subtraction
+subtract_complex <- complex_num_1 - complex_num_2
+cat("Subtraction of complex numbers:", subtract_complex, "\n")
+
+# multiplication
+multiply_complex <- complex_num_1 * complex_num_2
+cat("Multiplication of complex numbers:", multiply_complex, "\n")
+
+# division
+divide_complex <- complex_num_1 / complex_num_2
+cat("Division of complex numbers:", divide_complex, "\n")
